26466504afa978a65a53705059c9d10933b7c5d8
Honken <henrik@bastakompisar.se>
2017-06-02 15:16:05 +0200

    fixed line endings here also

--- logout.php
+++ logout.php
@@ -1,33 +1,33 @@
-<?php session_start() ?>
-
-<!doctype html>
-<html lang="sv">
-    <head>
-	<meta charset="UTF-8" />
-        <meta name="author" content="Henrik Johansson" />
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
-        <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
-	<title>Henke</title>
-    </head>
-
-    <body>
-	
-	<a href="index.php">Tillbaka till index</a>
-	
-	<?php
-        
-        
-	    $_SESSION=array(); // Sessionen lever fortfarande, men alla sessionsvariabler tas bort.
-	    session_destroy(); // Avslutar sessionen.
-        
-        
-        ?>
-        
-    </body>
-
-    <footer>
-        
-    </footer>
-    
+<?php session_start() ?>
+
+<!doctype html>
+<html lang="sv">
+    <head>
+	<meta charset="UTF-8" />
+        <meta name="author" content="Henrik Johansson" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+	<title>Henke</title>
+    </head>
+
+    <body>
+	
+	<a href="index.php">Tillbaka till index</a>
+	
+	<?php
+        
+        
+	    $_SESSION=array(); // Sessionen lever fortfarande, men alla sessionsvariabler tas bort.
+	    session_destroy(); // Avslutar sessionen.
+        
+        
+        ?>
+        
+    </body>
+
+    <footer>
+        
+    </footer>
+    
 </html>
